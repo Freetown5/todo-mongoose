40d392a4cfdb0d236c4b09ca04523c5a4a7478b4
Isa Conteh <iconteh21@gmail.com>
2020-10-12 20:59:36 -0400

    disabled etag because it was caused a 304 response

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const app = express();
 
 // app.use("/static", express.static("public"));
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
+app.disable('etag'); // causes 304: not modified response, not sure what it's use for. Something related to caching?
 
 app.get('/', (req, res) => {
     // res.send('Hello World!');
     res.render('todo.ejs');
+    console.log(res.statusCode);
 });
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+})
 
 app.listen(3000, () => console.log("Server Up and running"));
