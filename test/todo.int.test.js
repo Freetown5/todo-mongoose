1eb6f597c2da60308f5bd78ebb39afb291a375d3
Isa Conteh <iconteh21@gmail.com>
2020-10-15 19:24:00 -0400

    added a not working post test

--- test/todo.int.test.js
+++ test/todo.int.test.js
@@ -1,9 +1,20 @@
 const request = require("supertest");
 const app = require("../index");
+const task = require("../test/mocks/task.json");
 
 describe('/', () => {
-    test("GET" + '/', async () => {
+    test('/', async () => {
         const response = await request(app).get('/');
         expect(response.statusCode).toBe(200);
+    });
+
+    it("POST" + '/', async () => {
+        const response = await request(app)
+        .post('/')
+        .send(task);
+        expect(response.statusCode).toBe(201);
+        expect(response.body.title).toBe(newTodo.title);
+        expect(response.body.done).toBe(newTodo.done);
+        newTodoId = response.body._id;
     });
 });
